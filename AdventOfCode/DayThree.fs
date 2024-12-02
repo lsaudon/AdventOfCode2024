32fd6d93c4780e897e22a73edb08aa5167e8745a
lsaudon <lsaudon@gmail.com>
2024-12-02 23:03:54 +0100

    Ajout du module DayThree et tests associés
    Ajout du fichier `DayThree.fs` au projet `AdventOfCode.fsproj`.
    Création du module `DayThree` avec les fonctions `partOne` et `partTwo`.
    Ajout de la classe de test `DayThreeTest` pour tester ces fonctions.
    Correction des appels de test pour utiliser `DayThree` au lieu de `DayTwo`.

---
+++ AdventOfCode/DayThree.fs
@@ -0,0 +1,30 @@
+﻿namespace AdventOfCode
+open System
+open Microsoft.VisualStudio.TestTools.UnitTesting
+
+module DayThree =
+    let partOne (input: string) = 0
+    let partTwo (input: string) = 0
+
+[<TestClass>]
+type DayThreeTest () =
+
+    let exampleInput = """
+0
+"""
+    let puzzleInput = """
+0
+"""
+
+    [<TestMethod>]
+    member this.PartOneExample () =
+        Assert.AreEqual<int>(0, DayTwo.partOne exampleInput)
+    [<TestMethod>]
+    member this.PartOnePuzzle () =
+        Assert.AreEqual<int>(0,  DayTwo.partOne puzzleInput)
+    [<TestMethod>]
+    member this.PartTwoExample () =
+        Assert.AreEqual<int>(0, DayTwo.partTwo exampleInput)
+    [<TestMethod>]
+    member this.PartTwoPuzzle () =
+        Assert.AreEqual<int>(0, DayTwo.partTwo puzzleInput)
