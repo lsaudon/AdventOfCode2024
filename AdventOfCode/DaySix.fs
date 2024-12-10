14e3f5e0f5417feb6c1433232426ec5523ce4862
Lucas SAUDON <lucas.saudon@beta.gouv.fr>
2024-12-10 12:36:51 +0100

    Day6 init

---
+++ AdventOfCode/DaySix.fs
@@ -0,0 +1,37 @@
+﻿namespace AdventOfCode
+
+open System
+open Microsoft.VisualStudio.TestTools.UnitTesting
+
+module DaySix =
+    let partOne (input: string) : int =
+        0
+    
+    let partTwo (input: string) : int =
+        0
+
+[<TestClass>]
+type DaySixTest() =
+  let exampleInput =
+    """
+"""
+
+  let puzzleInput =
+    """
+"""
+
+  [<TestMethod>]
+  member this.PartOneExample() =
+    Assert.AreEqual<int>(0, DaySix.partOne exampleInput)
+
+  [<TestMethod>]
+  member this.PartOnePuzzle() =
+    Assert.AreEqual<int>(0, DaySix.partOne puzzleInput)
+
+  [<TestMethod>]
+  member this.PartTwoExample() =
+    Assert.AreEqual<int>(0, DaySix.partTwo exampleInput)
+
+  [<TestMethod>]
+  member this.PartTwoPuzzle() =
+    Assert.AreEqual<int>(0, DaySix.partTwo puzzleInput)
---
+++ AdventOfCode/DayTemplate.fs
@@ -0,0 +1,37 @@
+﻿namespace AdventOfCode
+
+open System
+open Microsoft.VisualStudio.TestTools.UnitTesting
+
+module DayTemplate =
+    let partOne (input: string) : int =
+        0
+    
+    let partTwo (input: string) : int =
+        0
+
+[<TestClass>]
+type DayTemplateTest() =
+  let exampleInput =
+    """
+"""
+
+  let puzzleInput =
+    """
+"""
+
+  [<TestMethod>]
+  member this.PartOneExample() =
+    Assert.AreEqual<int>(0, DayTemplate.partOne exampleInput)
+
+  [<TestMethod>]
+  member this.PartOnePuzzle() =
+    Assert.AreEqual<int>(0, DayTemplate.partOne puzzleInput)
+
+  [<TestMethod>]
+  member this.PartTwoExample() =
+    Assert.AreEqual<int>(0, DayTemplate.partTwo exampleInput)
+
+  [<TestMethod>]
+  member this.PartTwoPuzzle() =
+    Assert.AreEqual<int>(0, DayTemplate.partTwo puzzleInput)
